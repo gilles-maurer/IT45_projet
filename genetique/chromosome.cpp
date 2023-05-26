5de721c09c8334c3a37d149f1c08a387f53572f9
AugustinAth <athane.augustin@utbm.fr>
2023-05-26 11:27:28 +0200

    add classes chromosome and population

--- genetique/chromosome.cpp
+++ genetique/chromosome.cpp
@@ -1,5 +1,42 @@
 #include "chromosome.h"
 
 
+Chromosome::Chromosome(int tc){
 
+}
+
+Chromosome::~Chromosome(){// destructeur de l'objet
+
+}
+
+// METHODES
+void Chromosome::afficher(){// fonction d'affichage du Chromosome (i.e. de la solution)
+
+}   
+
+void Chromosome::evaluer(){
+
+}
+	                    // fonction d'évaluation du Chromosome (c-é-d calcul la fitness)
+                        //   Elle doit etre lancée é la creation des solution et apres
+					    //   l'exécution des operateurs de mutation et de croisement
+
+void Chromosome::ordonner(){  // ordonne le sens de la tournée si gene[1] > gene[taille-1]
+
+} 
+void Chromosome::copier(Chromosome* source){ // copie le Chromosome 'source'
+
+}
+
+bool Chromosome::identique(Chromosome* chro){ // test si 2 chromosome sont identique
+
+
+}
+
+// OPERATEURS DE MUTATION
+void Chromosome::mutation(){
+    srand(time(0));
+    int mutedGene = rand() % this->taille;
+    
+} 
 
--- genetique/population.cpp
+++ genetique/population.cpp
@@ -1,1 +1,60 @@
 #include "population.h"
+
+
+// CONSTRUCTEURS
+population::population(int tp, int tc){
+    this->taille_pop = tp;
+    this->tc = tc;
+
+}
+
+// destructeur de l'objet
+population::~population(){ 
+
+}            
+
+// METHODES
+
+// affiche quelques statistiques sur la population
+void population::statistiques(){
+
+} 
+
+// compte le nombre de Chromosomes similaires 'chro'
+int population::nb_Chromosomes_similaires(Chromosome* chro){
+    int count;
+
+    return count;
+
+}
+
+// affiche les resultats du comptage de Chromosomes similaires aux meilleurs individus de la population.                   
+void population::similitude(){
+
+}
+
+// OPERATEURS DE SELECTION ET DE REMPLACEMENT
+
+// selection par roulette biaisee d'un individu de la population
+Chromosome *population::selection_roulette(){
+
+}     
+
+// rempacement par roulette biaisee d'un individu de la population par un Chromosome donne
+void population::remplacement_roulette(Chromosome *individu){
+
+}
+    
+void population::ordonner(){
+
+}
+
+void population::reordonner(){
+
+}
+
+void population::afficher(){
+    for(int i = 0; i < taille_pop; i++){
+        individus
+    }
+}
